dac1100de67422bff4933609a01044379998dc83
hoangvv <hoangvv@techlab.ai>
2022-02-23 16:58:57 +0700

    add evironment configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,22 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:trading_project/configs/constants.dart';
 
+import 'configs/service_api_config.dart';
 import 'lang/translation_service.dart';
 import 'routes/app_pages.dart';
 import 'shared/logger/logger_utils.dart';
 
-void main() {
+
+
+Future<void> mainCommon(EnvironmentConfiguration serverConfig) async {
+  Environment().initConfig(serverConfig);
+  log(Environment().config.toString());
   runApp(const MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
