5fbc0333e25410cd07451d01414a2197cf7f3214
tienbm_funtap <tienbm@mobgame.vn>
2022-03-03 16:38:38 +0700

    -

--- lib/pages/main_tabbar/main_tabbar_binding.dart
+++ lib/pages/main_tabbar/main_tabbar_binding.dart
@@ -1,14 +1,13 @@
 import 'package:get/instance_manager.dart';
 import 'package:trading_module/pages/homePage/controller/home_page_controller.dart';
 import 'package:trading_module/pages/main_tabbar/main_tabbar_controller.dart';
-import 'package:trading_module/pages/on_boarding/intro/intro_controller.dart';
 
 
 class MainBinding extends Bindings {
   @override
   void dependencies() {
     Get.lazyPut(() => HomePageController());
-    Get.lazyPut(() => IntroController(userIsRegisteredKyc:true, userIsRegisteredOTP: true));
+    // Get.lazyPut(() => IntroController(userIsRegisteredKyc:true, userIsRegisteredOTP: true));
     Get.lazyPut(() => MainTabController());
   }
 }
