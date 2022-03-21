06768220f327fee55af1deee1df01a0b2d200486
hoangvv <hoangvv@techlab.ai>
2022-03-21 10:59:07 +0700

    remove code

--- lib/pages/deposit/transfer_type/transfer_type_scene.dart
+++ lib/pages/deposit/transfer_type/transfer_type_scene.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
-import 'package:trading_module/pages/deposit/transfer_type/component/bank_type.dart';
 import 'package:trading_module/pages/deposit/transfer_type/component/nine_pay_type.dart';
-import 'package:trading_module/pages/deposit/transfer_type/component/saving_product_type.dart';
 import 'package:trading_module/pages/support_fab/support_fab.dart';
 import 'package:trading_module/shared_widgets/BaseScaffold.dart';
 import 'package:trading_module/utils/extensions.dart';
