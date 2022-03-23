02b783c8b6fb64ffd7bf34f46202005636eed15f
hoangvv <hoangvv@techlab.ai>
2022-03-23 14:40:41 +0700

    merge code

--- lib/pages/homePage/home_page_bindding.dart
+++ lib/pages/homePage/home_page_bindding.dart
@@ -1,8 +1,11 @@
 import 'package:get/get.dart';
 import 'package:trading_module/data/repos/home_trading_repo_imp.dart';
+import 'package:trading_module/data/repos/withdraw_repos_impl.dart';
 import 'package:trading_module/data/services/home_trading_service.dart';
+import 'package:trading_module/data/services/withdraw_service.dart';
 import 'package:trading_module/domain/repos/home_trading_repo.dart';
 import 'package:trading_module/domain/use_cases/home_trading_usecase.dart';
+import 'package:trading_module/domain/use_cases/open_withdraw_usecase.dart';
 import 'package:trading_module/pages/homePage/controller/home_page_controller.dart';
 
 import 'views/PropertyView/property_controller.dart';
@@ -12,6 +15,9 @@ class HomePageBinding extends Bindings {
   void dependencies() {
     Get.lazyPut<HomeTradingRepo>(() => HomeTradingRepoImpl(HomeTradingServiceImpl()));
     Get.lazyPut(() => HomePropertyController());
+
+    Get.lazyPut(() =>  OpenWithdrawUseCase(WithdrawRepoImpl(WithdrawServiceImpl())));
+    Get.lazyPut(() => HomePageController());
 
     Get.lazyPut<HomeTradingRepo>(() => HomeTradingRepoImpl(HomeTradingServiceImpl()));
     Get.lazyPut<HomeTradingUseCase>(() => HomeTradingUseCase(Get.find()));
