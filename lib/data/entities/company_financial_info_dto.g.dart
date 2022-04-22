0590b672702be9d10ceb3b51d44b415cf424d578
tienbm_funtap <tienbm@mobgame.vn>
2022-04-22 17:49:48 +0700

    //update model company financial info

---
+++ lib/data/entities/company_financial_info_dto.g.dart
@@ -0,0 +1,73 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'company_financial_info_dto.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+CompanyFinancialInfoDto _$CompanyFinancialInfoDtoFromJson(
+        Map<String, dynamic> json) =>
+    CompanyFinancialInfoDto(
+      symbol: json['symbol'] as String?,
+      revenue: json['revenue'] as int?,
+      cost: json['cost'] as int?,
+      taxIncome: json['taxIncome'] as int?,
+      profit: json['profit'] as int?,
+      totalAssets: json['totalAssets'] as int?,
+      operationCost: json['operationCost'] as int?,
+      depositCredit: json['depositCredit'] as int?,
+      depositClient: json['depositClient'] as int?,
+      debt: json['debt'] as int?,
+      debtCredit: json['debtCredit'] as int?,
+      debtClient: json['debtClient'] as int?,
+      capital: json['capital'] as int?,
+      capitalCredit: json['capitalCredit'] as int?,
+      undistProfit: json['undistProfit'] as int?,
+      valuation: json['valuation'] as int?,
+      eps: json['eps'] as int?,
+      pe: json['pe'] as int?,
+      profitability: json['profitability'] as int?,
+      roe: json['roe'] as int?,
+      roa: json['roa'] as int?,
+      poic: json['poic'] as int?,
+      grossProfitMargin: json['grossProfitMargin'] as int?,
+      netProfitVariable: json['netProfitVariable'] as int?,
+      financialStrength: json['financialStrength'] as int?,
+      debtEquity: json['debtEquity'] as int?,
+      debtAssets: json['debtAssets'] as int?,
+      fastPayment: json['fastPayment'] as int?,
+    );
+
+Map<String, dynamic> _$CompanyFinancialInfoDtoToJson(
+        CompanyFinancialInfoDto instance) =>
+    <String, dynamic>{
+      'symbol': instance.symbol,
+      'revenue': instance.revenue,
+      'cost': instance.cost,
+      'taxIncome': instance.taxIncome,
+      'profit': instance.profit,
+      'totalAssets': instance.totalAssets,
+      'operationCost': instance.operationCost,
+      'depositCredit': instance.depositCredit,
+      'depositClient': instance.depositClient,
+      'debt': instance.debt,
+      'debtCredit': instance.debtCredit,
+      'debtClient': instance.debtClient,
+      'capital': instance.capital,
+      'capitalCredit': instance.capitalCredit,
+      'undistProfit': instance.undistProfit,
+      'valuation': instance.valuation,
+      'eps': instance.eps,
+      'pe': instance.pe,
+      'profitability': instance.profitability,
+      'roe': instance.roe,
+      'roa': instance.roa,
+      'poic': instance.poic,
+      'grossProfitMargin': instance.grossProfitMargin,
+      'netProfitVariable': instance.netProfitVariable,
+      'financialStrength': instance.financialStrength,
+      'debtEquity': instance.debtEquity,
+      'debtAssets': instance.debtAssets,
+      'fastPayment': instance.fastPayment,
+    };
---
+++ lib/domain/entities/company_financial_info.dart
@@ -0,0 +1,62 @@
+class CompanyFinancialInfo {
+  String symbol;
+  int revenue;
+  int cost;
+  int taxIncome;
+  int profit;
+  int totalAssets;
+  int operationCost;
+  int depositCredit;
+  int depositClient;
+  int debt;
+  int debtCredit;
+  int debtClient;
+  int capital;
+  int capitalCredit;
+  int undistProfit;
+  int valuation;
+  int eps;
+  int pe;
+  int profitability;
+  int roe;
+  int roa;
+  int poic;
+  int grossProfitMargin;
+  int netProfitVariable;
+  int financialStrength;
+  int debtEquity;
+  int debtAssets;
+  int fastPayment;
+
+  CompanyFinancialInfo(
+      {required this.symbol,
+      required this.revenue,
+      required this.cost,
+      required this.taxIncome,
+      required this.profit,
+      required this.totalAssets,
+      required this.operationCost,
+      required this.depositCredit,
+      required this.depositClient,
+      required this.debt,
+      required this.debtCredit,
+      required this.debtClient,
+      required this.capital,
+      required this.capitalCredit,
+      required this.undistProfit,
+      required this.valuation,
+      required this.eps,
+      required this.pe,
+      required this.profitability,
+      required this.roe,
+      required this.roa,
+      required this.poic,
+      required this.grossProfitMargin,
+      required this.netProfitVariable,
+      required this.financialStrength,
+      required this.debtEquity,
+      required this.debtAssets,
+      required this.fastPayment});
+
+
+}
