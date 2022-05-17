ea850954c5f963c7ff20c54c6f96dc5b77bf19b8
ducanh <anhnmd@techlab.ai>
2022-05-17 11:58:37 +0700

    add case onEmpty news stock

--- lib/pages/stock_more_detail/tabs/news/stock_company_news_view.dart
+++ lib/pages/stock_more_detail/tabs/news/stock_company_news_view.dart
@@ -17,27 +17,33 @@ class StockCompanyNewsView extends StatelessWidget {
   Widget build(BuildContext context) {
     final StockCompanyNewsController controller = Get.find();
     return controller.obx(
-      (newsList) {
-        return ListView.builder(
-          itemBuilder: (context, index) =>
-              CompanyNewsItemView(news: newsList![index]),
-          shrinkWrap: true,
-          physics: const ClampingScrollPhysics(),
-          itemCount: newsList?.length,
-        );
-      },
-      onLoading: const Padding(
-        padding: EdgeInsets.only(top: 120),
-        child: Center(
-          child: CircularProgressIndicator(),
+            (newsList) {
+          return ListView.builder(
+            itemBuilder: (context, index) =>
+                CompanyNewsItemView(news: newsList![index]),
+            shrinkWrap: true,
+            physics: const ClampingScrollPhysics(),
+            itemCount: newsList?.length,
+          );
+        },
+        onLoading: const Padding(
+          padding: EdgeInsets.only(top: 120),
+          child: Center(
+            child: CircularProgressIndicator(),
+          ),
         ),
-      ),
-      onError: (error) => const ListNoDataBackground(
-        padding: EdgeInsets.only(top: 100),
-        pngPath: "assets/images/png/trans_empty.png",
-        title: "Chưa có dữ liệu ",
-        desc: "",
-      ),
+        onError: (error) => const ListNoDataBackground(
+          padding: EdgeInsets.only(top: 100),
+          pngPath: "assets/images/png/trans_empty.png",
+          title: "Có lỗi xảy ra!",
+          desc: "",
+        ),
+        onEmpty: const ListNoDataBackground(
+          padding: EdgeInsets.only(top: 100),
+          pngPath: "assets/images/png/trans_empty.png",
+          title: "Chưa có tin tức ",
+          desc: "Cổ phiếu chưa có tin tức nào",
+        )
     );
   }
 }
